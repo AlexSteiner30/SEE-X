ccfd04d08a7032f788cc4e30f41d6377c5431628
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-10-08 09:52:30 +0200

    1

--- predict.py
+++ predict.py
@@ -3,21 +3,19 @@ from dataset import *
 
 dataset = LoadDataset()
 
-clf = ImageClassifier()#.to('cuda')
+clf = ImageClassifier().to('cuda')
 opt = Adam(clf.parameters(), lr=1e-3)
 loss_fn = nn.CrossEntropyLoss() 
-
-with open('static/model_state.pt', 'wb') as f: 
-    save(clf.state_dict(), f) 
 
 with open('static/model_state.pt', 'rb') as f: 
     clf.load_state_dict(load(f))  
 
 def predict(x):
-    img = Image.open(x) 
-    img_tensor = ToTensor()(img).unsqueeze(0).reshape(1,1, 369,369)#.to('cuda')
+    img = Image.open(x)  
+    img_tensor = ToTensor()(img).reshape(1,1,369,369).to('cuda')
 
     return torch.argmax(clf(img_tensor)).item()
 
-x = input()
-print(predict('dataset/' + str(x) + '.png'))
+while True:
+    x = input()
+    print(predict('dataset/' + str(x) + '.png'))
